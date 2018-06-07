f61b808138059ddfbfdd1c17c3db5592fa0cb72c
60wanjinbing <zhuiqiuzhe1989@live.cn>
2018-06-07 20:28:32 +0800

    Update dispatchservlet.java

--- dispatchservlet.java
+++ dispatchservlet.java
@@ -9,3 +9,6 @@ protected void initStrategies(ApplicationContext context) {
 		initViewResolvers(context);
 		initFlashMapManager(context);
 	}
+HandlerExecutionChain mappedHandler
+DispatchSerlet中doDispatch中，是先依据request找到mappinghandle,再依据mappingHandle找到处理的Adapter，最后由handleAdapter，
+去处理Controler中的method，返回ModelAndView,继续往下执行。
